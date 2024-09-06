08b6da9b1827d430d461778ef65cd84bad7b499a
GiuGiu131 <giugiu.cmm@gmail.com>
2024-09-06 12:08:51 +0100

    configure redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Routes from "./routes.jsx";
+import Routes from "./routes";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </StrictMode>
 );
